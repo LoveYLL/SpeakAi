19a87d14c81b2df8f8feed901869e2886b4466e7
星辰 <2823156835@qq.com>
2025-07-18 20:34:12 +0800

    项目构建

---
+++ src/main/java/comy/SpeakApp.java
@@ -0,0 +1,12 @@
+package comy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakApp.class, args);
+    }
+}
